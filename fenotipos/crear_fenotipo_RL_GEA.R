1a0dbe380fbda691c993779c185e91efab57f434
luis macias <luisrmacias@gmail.com>
2018-09-03 18:14:11 -0500

    agregamos archivos de fenotipos pero les falta generabilidad

---
+++ fenotipos/crear_fenotipo_RL_GEA.R
@@ -0,0 +1,73 @@
+library(MASS)
+library(stringr)
+library(foreign)
+
+setwd("~/analisis_asociacion/adultos_mega")
+load("cirugia_bar/fenotipos/histologia_revisada.RData")
+genotipos <- "cirugia_bar/imputados/fusionados/fusion_agosto2018.QC"
+ids <- read.table(paste0(genotipos, ".fam"))
+ids <- ids[grepl("RL|GEA", ids$V2),]
+ids$V2[!(ids$V2 %in% morbidos$IID)]
+##RL_109, RL_252, RL_253, RL_258, RL_282, RL436 y RL_441 se genotiparon tanto con y sin guión bajo. Se cambia IID (se retira guión bajo).
+morbidos$IID[grep("252|253|258|109|282|436|441", morbidos$IID)] <- gsub("_", "", morbidos$IID[grep("252|258|253|109|282|436|441", morbidos$IID)])
+
+recientes <- read.spss("cirugia_bar/fenotipos/SPSS/metabolómica_2aetapa.sav", to.data.frame=TRUE)
+recientes$ID <- str_trim(recientes$ID)
+recientes$FECHA_NACIMIENTO <- as.Date(recientes$FECHA_NACIMIENTO, "%d/%m/%Y")
+names(recientes)[!names(recientes) %in% names(morbidos)]
+names(recientes)[grep("[0-9]\\.[1-5]", names(recientes))] <- gsub("\\.", "", grep("[0-9]\\.[1-5]", names(recientes), value=TRUE))
+names(recientes)[grep("sexo", names(recientes))] <- "GENERO"
+recientes$IID <- recientes$ID
+recientes <- recientes[-(212:213),]
+recientes <- recientes[names(recientes) %in% names(morbidos)]
+recientes[pmatch(names(morbidos), names(recientes))]
+agregar <- matrix(NA, nrow(recientes), ncol(morbidos[!names(morbidos) %in% names(recientes)]))
+colnames(agregar) <- names(morbidos)[!names(morbidos) %in% names(recientes)]
+recientes <- cbind(recientes, data.frame(agregar))
+
+morbidos <- rbind(morbidos, recientes[pmatch(names(morbidos), names(recientes))])
+rm(agregar, genotipos, ids, recientes)
+save.image("cirugia_bar/fenotipos/histologia_revisada.RData")
+
+grep("IID", names(morbidos))
+##2
+grep("EDAD", names(morbidos))
+##33
+grep("IMC", names(morbidos))
+##40
+grep("GENERO", names(morbidos))
+##34
+
+##primer archivo de covariables generales
+covar <- read.table("fenotipos/GCTA/covar.cov", header=TRUE)
+covar <- covar[!covar$IID %in% morbidos$IID,]
+covar <- rbind(covar, with(morbidos[morbidos$IID!="",], data.frame(FID=0,  IID=IID, EDAD=EDAD, IMC=IMC)))
+write.table(covar, "fenotipos/GCTA/covar.cov", row.names=FALSE, quote=FALSE)
+
+amino <- 183:193
+carni <- 194:225
+
+amino <- morbidos[, amino]
+
+##3ARG raíz cuadrada/4CIT log/5GLY logaritmo/6ALA logaritmo/7LEU raíz cuadrada/8MET log/9PHE raíz cuadrada/10TYR raíz cuadrada/11VAL igual/12ORN logaritmo/13PRO raíz cuadrada
+
+amino <- data.frame(FID=0, IID=morbidos$IID[complete.cases(amino)], amino[complete.cases(amino),])
+amino <- amino[amino$IID!="",]
+amino[,c(3,7,9,10,13)] <- sqrt(amino[,c(3,7,9,10,13)])
+amino[,c(4, 5, 6, 8, 12)] <- log(amino[,c(4, 5, 6, 8, 12)])
+write.table(amino, "cirugia_bar/fenotipos/para_asoc/aminoacidos_transformados.phen", row.names=FALSE, quote=FALSE)
+
+
+carni <- morbidos[, carni]
+raiz <- c("SA", "C0", "C5OHC4DC", "C5DCC6OH", "C5", "C102")
+inverso <- c("C6DC", "C101")
+loga <- c("C3", "C4", "C51", "C6", "C8", "C81", "C161", "C16OH", "C10", "C14", "C18")
+raiz_inversa <- c("C2", "C4OHC3DC", "C16", "C12", "C121", "C141", "C142", "C14OH", "C181", "C182")
+
+carni[,names(carni) %in% raiz] <- sqrt(carni[,names(carni) %in% raiz])
+carni[,names(carni) %in% inverso] <- 1/carni[,names(carni) %in% inverso]
+carni[,names(carni) %in% loga] <- log(carni[,names(carni) %in% loga])
+carni[,names(carni) %in% raiz_inversa] <- carni[,names(carni) %in% raiz_inversa]^-0.5
+
+carni <- data.frame(FID=0, IID=morbidos$IID, carni)
+write.table(carni, "cirugia_bar/fenotipos/para_asoc/carnitinas_transformadas.phen", row.names=FALSE, quote=FALSE)
