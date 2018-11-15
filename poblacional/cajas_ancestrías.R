570091a6531be8ef5b63db26626003625831ea18
luis macias <luisrmacias@gmail.com>
2018-11-15 13:21:19 -0600

    seguimos con generalización, hacemos algunos cambios en guiones de gráficas

--- poblacional/cajas_ancestrías.R
+++ poblacional/cajas_ancestrías.R
@@ -1,30 +1,30 @@
 library(ggplot2)
 library(broom)
 carpeta <- "~/analisis_asociacion/adultos_mega/calidad/cluster3"
-estudio <- "ancestrales_adultos_indep"
+estudio <- "ancestrales.niños.indep"
 setwd(carpeta)
 ances <- read.table(paste0(estudio,".3.Q"))
 ances <- cbind(read.table(paste0(estudio, ".fam"))[c(1:2)], ances)
+names(ances)[1:2] <- c("FID", "IID")
 ids_ceu <- scan("~/CANDELA/haplotipos/ref_beagle/listas/grupos/fundadores_CEU.txt", what='list')
 ids_yri <- scan("~/CANDELA/haplotipos/ref_beagle/listas/grupos/fundadores_YRI.txt", what='list')
-indigenas <- grepl("FN|TOT|ZAP|NFM", ances$V2)
+indigenas <- grepl("FN|TOT|ZAP|NFM", ances$IID)
 summary(ances[ances$V2 %in% ids_ceu,3:5])
-##V3 CEU
+##V1 CEU
 columna_CEU <- which(apply(ances[ances$IID %in% ids_ceu,3:5], 2, median)>0.95)
-summary(ances[ances$V2 %in% ids_yri,3:5])
+summary(ances[ances$IID %in% ids_yri,3:5])
 ##V2 YRI
 columna_YRI <- which(apply(ances[ances$IID %in% ids_yri,3:5], 2, median)>0.95)
 summary(ances[indigenas,3:5])
-##V1 NAT
-summary(ances[!indigenas & !grepl("NA", ances$V2),3:5])
+##V3 NAT
+summary(ances[!indigenas & !grepl("NA", ances$IID),3:5])
 columna_NAT <- which(apply(ances[indigenas,3:5], 2, median)>0.95)
 
-names(ances)[1:2] <- c("FID", "IID")
 names(ances)[c(columna_NAT, columna_CEU, columna_YRI)+2] <- c("NAT", "CEU", "YRI")
 mestizos <- ances[!indigenas & !grepl("NA", ances$IID),]
-	tst <- wilcox.test(mestizos$CEU ~ grepl("GC|ICN", mestizos$IID))
+	tst <- wilcox.test(mestizos$CEU ~ factor(mestizos$IID %in% status[status[,6]==1,2]))
 	pval = tidy(tst)$p.value
-	plt1 <- ggplot(mestizos, aes(x=grepl("GC|ICN", mestizos$IID), y=NAT))+ 
+	plt1 <- ggplot(mestizos, aes(x=mestizos$IID %in% status[status[,6]==1,2], y=NAT))+ 
 	geom_boxplot()
 	plt1 + geom_text(aes(x=0.7, y = 0.82, label=paste('P-value:', format.pval(pval, digits=2))))
 boxplot(mestizos$CEU ~ grepl("GC|ICN", mestizos$IID), col=c("honeydew3", "indianred"), notch=TRUE, names=c("Casos", "Controles"), ylab="Proporción de componente ancestral europeo", cex.lab=1.15, cex.axis=1.15)
--- poblacional/gráfica_componentes.R
+++ poblacional/gráfica_componentes.R
@@ -1,20 +1,25 @@
-carpeta <- "ancestrales_adultos_imputados.mds"
+carpeta <- "ancestrales.niños.indep.mds"
 pca <- read.table(carpeta, header=TRUE)
 ids_ceu <- scan("~/CANDELA/haplotipos/ref_beagle/listas/grupos/fundadores_CEU.txt", what='list')
 ids_yri <- scan("~/CANDELA/haplotipos/ref_beagle/listas/grupos/fundadores_YRI.txt", what='list')
-por_centro <- read.table("../../calidad/cluster3/grupo_asig.txt")
+por_centro <- read.table("../grupo_asig.txt")
 europeos <- na.omit(pmatch(ids_ceu, pca$IID))
 yoruba <- na.omit(pmatch(ids_yri, pca$IID))
 indigenas <- grep("FN|TOT|ZAP|NFM", pca$IID)
 ref <- c(indigenas, europeos, yoruba)
-table(substr(pca$IID[-ref], 1, 3))
+status <- read.table("~/analisis_asociacion/niños/niños/imputados/fusionados/fusion_noviembre2018.QC.fam")
+casos <- which(pca$IID %in% status[status[,6]==2,2])
+controles <- which(pca$IID %in% status[status[,6]==1,2])
+desconocidos <- which(pca$IID %in% status[status[,6]==-9,2])
 medix <- grep("MZ1|CON|SOL|RED|TER", pca$IID)
 qx_ob <- grep("RL|GEA", pca$IID)
 controles <- grep("GC|ICN", pca$IID)        
 pob <- character(dim(pca)[1])
 pob[medix] <- "Medix"
 pob[qx_ob] <- "Cirugía bariátrica"
+pob[casos] <- "Con obesidad"
 pob[controles] <- "Normopeso"
+pob[desconocidos] <- "Otro estado nutricio"
 pob[europeos] <- "CEU"
 pob[indigenas] <- "Indígenas"
 pob[yoruba] <- "YRI"
@@ -23,9 +28,9 @@ pob[pob==""] <- NA
 pob <- factor(pob)
 pca <- cbind(pob, pca)
 rm(pob)
-pca <- pca[pca$pob!="",] 
-jpeg('mds_porestudio.jpg')
-	plot(pca$PC1, pca$PC2, col=c("turquoise", "orange","green","black","purple","brown")[pca$pob], pch=18, xlab="First principal component", ylab="Second principal component")
+pca <- pca[pca$pob!="",]
+jpeg('mds_porestadonutricio.jpg')
+	plot(pca$C1, pca$C2, col=c("turquoise", "orange","green","black","purple","brown")[pca$pob], pch=18, xlab="First multidimensional scaling component", ylab="Second multidimensional scaling component")
         ##title(main="Distribución de componentes principales de los individuos\ndel estudio con respecto a poblaciones ancestrales")
-        legend(-0.08, -0.055, levels(pca$pob), bg="white", col=c("turquoise", "orange","green","black","purple","brown"), pch=18, pt.cex=2, y.intersp=1.15)
+        legend(-0.12, -0.03, levels(pca$pob), bg="white", col=c("turquoise", "orange","green","black","purple","brown"), pch=18, pt.cex=2, y.intersp=1.15)
 	dev.off()
