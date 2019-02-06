0751ef725d41d3a5678657905083b1189e2575ea
luis macias <luisrmacias@gmail.com>
2019-02-05 18:39:54 -0600

    cambios mínimos

--- preparacion/sitios_dup_bim.R
+++ preparacion/sitios_dup_bim.R
@@ -5,5 +5,5 @@ chr_pos <- snp[,c(1,4)]
 dobles <- snp[duplicated(chr_pos),2]
 dobles <- dobles[which(dobles!=levels(dobles)[1])]
 dobles <- droplevels(dobles)
-write.table(dobles, "listas/eliminacion_QC/SNP_duplicados.list", row.names=FALSE, col.names=FALSE, quote=FALSE)
+write.table(dobles, "~/analisis_asociacion/adultos_mega/listas/eliminacion_QC/SNP_duplicados.list", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
